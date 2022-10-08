305b0f0781c7492cba2d5b7189a0f58e1f7206b7
king stark <63186186+King-stark@users.noreply.github.com>
2022-10-08 22:49:18 +0800

    Update goorm-onekey.sh

--- config/goorm-onekey.sh
+++ config/goorm-onekey.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 #升级并安装基础包管理工具
 apt-get -y update
-apt-get -y install nano curl wget bash-completion
+apt-get -y install nano curl wget bash-completion libevent-*
+apt-get -y update && apt-get -y autoremove && apt-get -y autoclean
 
 # 下载编译好的全功能sing-box文件
 wget -c https://pan.kodbox.ga/sing-box/sing-box.tar.gz

a892e58cbc81ae6b9d9902b8b8b70826bcdb77c1
king stark <63186186+King-stark@users.noreply.github.com>
2022-10-11 02:22:59 +0800

    Update goorm-onekey.sh

--- config/goorm-onekey.sh
+++ config/goorm-onekey.sh
@@ -5,11 +5,22 @@ apt-get -y install nano curl wget bash-completion libevent-*
 apt-get -y update && apt-get -y autoremove && apt-get -y autoclean
 
 # 下载编译好的全功能sing-box文件
-wget -c https://pan.kodbox.ga/sing-box/singbox.tar.gz
+wget -c https://pan.kodbox.ga/singbox/singbox.tar.gz
 tar -zxf singbox.tar.gz && chmod +x ./singbox && rm singbox.tar.gz
 
 #下载配置文件
 wget -c https://github.com/King-stark/singbox/raw/main/config/goorm-server.json -O config.json
 
-#运行
+#运行singbox服务
 nohup ./singbox run -c config.json >/dev/null 2>&1 &
+
+#测试 增加个web进程防止容器掉线
+#caddy运行 下载预编译库 https://github.com/lxhao61/integrated-examples.git
+wget -c https:// pan.kodbox.ga/singbox/caddy.tar.gz
+tar -zxf caddy.tar.gz && chmod +x ./caddy && rm caddy.tar.gz
+
+#下载Caddyfile
+wget -c https://github.com/King-stark/singbox/raw/main/config/Caddyfile
+
+#运行caddy服务
+./caddy start

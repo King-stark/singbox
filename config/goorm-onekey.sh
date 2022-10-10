19c65884a2c0680006578d7d4ff914bf0fd3323a
king stark <63186186+King-stark@users.noreply.github.com>
2022-10-11 01:49:17 +0800

    Update goorm-onekey.sh

--- config/goorm-onekey.sh
+++ config/goorm-onekey.sh
@@ -5,11 +5,11 @@ apt-get -y install nano curl wget bash-completion libevent-*
 apt-get -y update && apt-get -y autoremove && apt-get -y autoclean
 
 # 下载编译好的全功能sing-box文件
-wget -c https://pan.kodbox.ga/sing-box/sing-box.tar.gz
-tar -zxf sing-box.tar.gz && chmod +x ./sing-box && rm sing-box.tar.gz
+wget -c https://pan.kodbox.ga/sing-box/singbox.tar.gz
+tar -zxf singbox.tar.gz && chmod +x ./singbox && rm singbox.tar.gz
 
 #下载配置文件
 wget -c https://github.com/King-stark/singbox/raw/main/config/goorm-server.json -O config.json
 
 #运行
-nohup ./sing-box run -c config.json &
+nohup ./singbox run -c config.json >/dev/null 2>&1 &

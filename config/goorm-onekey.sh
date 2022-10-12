f069ffe7599ddb769a82373ea78bfc59d45961be
king stark <63186186+King-stark@users.noreply.github.com>
2022-10-12 08:57:38 +0800

    Update goorm-onekey.sh

--- config/goorm-onekey.sh
+++ config/goorm-onekey.sh
@@ -14,13 +14,18 @@ wget -c https://github.com/King-stark/singbox/raw/main/config/goorm-server.json 
 #运行singbox服务
 nohup ./singbox run -c config.json >/dev/null 2>&1 &
 
-#测试 增加个web进程防止容器掉线
-#caddy运行 下载预编译库 https://github.com/lxhao61/integrated-examples.git
-wget -c https:// pan.kodbox.ga/singbox/caddy.tar.gz
-tar -zxf caddy.tar.gz && chmod +x ./caddy && rm caddy.tar.gz
+# ~#测试 增加个web进程防止容器掉线~
+# #caddy运行 下载预编译库 https://github.com/lxhao61/integrated-examples.git
+# wget -c https:// pan.kodbox.ga/singbox/caddy.tar.gz
+# tar -zxf caddy.tar.gz && chmod +x ./caddy && rm caddy.tar.gz
 
-#下载Caddyfile
-wget -c https://github.com/King-stark/singbox/raw/main/config/Caddyfile
+# #下载mikutap网页代码
+# wget -c https://github.com/King-stark/singbox/raw/main/config/mikutap.tar.gz
+# tar -zxf mikutap.tar.gz
+
+# #下载Caddyfile
+# wget -c https://github.com/King-stark/singbox/raw/main/config/Caddyfile
+
 
 #运行caddy服务
 ./caddy start
